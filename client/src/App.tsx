ad5e6c4afc7f20d9bf1caf6e8a053c212bc6d0a9
Rabin Adhikari <rabin.adk1@gmail.com>
2022-11-17 17:11:58 +0545

    Do not load AdminPanel code at first

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,26 @@
+import { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router";
 import { BrowserRouter } from "react-router-dom";
 import Admin from "./components/Admin";
-import TweetCollectionAdminPanel from "./components/Admin/TweetCollectionAdminPanel";
 import EndUser from "./components/EndUser";
 import Login from "./components/Login";
+import NoMatch from "./components/NoMatch";
 import RequireAuth from "./components/RequireAuth";
 import AuthProvider from "./contexts/AuthProvider";
 import FilterProvider from "./contexts/FilterProvider";
+
+// Do not load AdminPanel code at first
+const TweetCollectionAdminPanel = lazy(
+  () => import("./components/Admin/TweetCollectionAdminPanel")
+);
 
 const App = () => (
   <AuthProvider>
     <FilterProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="*" element={<EndUser />} />
+          <Route index element={<EndUser />} />
           <Route path="/login" element={<Login />} />
-          {/* <Route path="/about" element={<About />} /> */}
           <Route
             path="/ap"
             element={
@@ -25,14 +30,23 @@ const App = () => (
             }
           >
             <Route
-              path="modify"
-              element={<TweetCollectionAdminPanel action="modify" />}
+              index
+              element={
+                <Suspense fallback="Loading">
+                  <TweetCollectionAdminPanel action="verify" />
+                </Suspense>
+              }
             />
             <Route
-              index
-              element={<TweetCollectionAdminPanel action="verify" />}
+              path="modify"
+              element={
+                <Suspense fallback="Loading">
+                  <TweetCollectionAdminPanel action="modify" />
+                </Suspense>
+              }
             />
           </Route>
+          <Route path="*" element={<NoMatch />} />
         </Routes>
       </BrowserRouter>
     </FilterProvider>
