710bebd6cefa025b466a8fdadb648850af4a6bde
Rabin Adhikari <rabin.adk1@gmail.com>
2023-01-17 15:24:57 +0545

    Add plugin to minify html

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { ViteMinifyPlugin } from "vite-plugin-minify";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ViteMinifyPlugin()],
   build: {
     outDir: "build",
     manifest: true,
